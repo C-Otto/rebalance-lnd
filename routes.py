7a130854ab4368e0eb426adf9f29c85346399be3
Carsten Otto <carsten.otto@andrena.de>
2019-01-04 14:54:27 -0500

    extract extension logic

--- routes.py
+++ routes.py
@@ -1,9 +1,8 @@
 import sys
-import rpc_pb2 as ln
+from route_extension import RouteExtension
 
 MAX_ROUTES_TO_REQUEST = 60
 ROUTE_REQUEST_INCREMENT = 15
-HIGH_FEES_THRESHOLD_MSAT = 3000000
 
 
 def debug(message):
@@ -20,6 +19,7 @@ class Routes:
         self.all_routes = []
         self.returned_routes = []
         self.num_requested_routes = 0
+        self.route_extension = RouteExtension(self.lnd, self.rebalance_channel, self.payment)
 
     def has_next(self):
         self.update_routes()
@@ -51,96 +51,7 @@ class Routes:
             self.add_route(modified_route)
 
     def add_rebalance_channel(self, route):
-        hops = route.hops
-        last_hop = hops[-1]
-        amount_msat = int(last_hop.amt_to_forward_msat)
-
-        expiry_last_hop = self.lnd.get_current_height() + self.get_expiry_delta_last_hop()
-        total_time_lock = expiry_last_hop
-
-        self.update_amounts(hops)
-        total_time_lock = self.update_expiry(hops, total_time_lock)
-
-        hops.extend([self.create_new_hop(amount_msat, self.rebalance_channel, expiry_last_hop)])
-
-        if self.update_route_totals(route, total_time_lock):
-            return route
-        else:
-            return None
-
-    @staticmethod
-    # Returns True only if everything went well, False otherwise
-    def update_route_totals(route, total_time_lock):
-        total_fee_msat = 0
-        for hop in route.hops:
-            total_fee_msat += hop.fee_msat
-        if total_fee_msat > HIGH_FEES_THRESHOLD_MSAT:
-            debug("High fees! " + str(total_fee_msat) + " msat")
-            return False
-
-        total_amount_msat = route.hops[-1].amt_to_forward_msat + total_fee_msat
-
-        route.total_amt_msat = total_amount_msat
-        route.total_amt = total_amount_msat // 1000
-        route.total_fees_msat = total_fee_msat
-        route.total_fees = total_fee_msat // 1000
-
-        route.total_time_lock = total_time_lock
-        return True
-
-    def create_new_hop(self, amount_msat, channel, expiry):
-        new_hop = ln.Hop(
-            chan_capacity=channel.capacity,
-            fee_msat=0,
-            fee=0,
-            expiry=expiry,
-            amt_to_forward_msat=amount_msat,
-            amt_to_forward=amount_msat // 1000,
-            chan_id=channel.chan_id,
-            pub_key=self.lnd.get_own_pubkey(),
-        )
-        return new_hop
-
-    def update_amounts(self, hops):
-        additional_fees = 0
-        for hop in reversed(hops):
-            amount_to_forward_msat = hop.amt_to_forward_msat + additional_fees
-            hop.amt_to_forward_msat = amount_to_forward_msat
-            hop.amt_to_forward = amount_to_forward_msat // 1000
-
-            fee_msat_before = hop.fee_msat
-            new_fee_msat = self.get_fee_msat(amount_to_forward_msat, hop.chan_id, hop.pub_key)
-            hop.fee_msat = new_fee_msat
-            hop.fee = new_fee_msat // 1000
-            additional_fees += new_fee_msat - fee_msat_before
-
-    def get_expiry_delta_last_hop(self):
-        return self.payment.cltv_expiry
-
-    def update_expiry(self, hops, total_time_lock):
-        for hop in reversed(hops):
-            hop.expiry = total_time_lock
-
-            channel_id = hop.chan_id
-            target_pubkey = hop.pub_key
-            policy = self.lnd.get_policy(channel_id, target_pubkey)
-
-            time_lock_delta = policy.time_lock_delta
-            total_time_lock += time_lock_delta
-        return total_time_lock
-
-    def get_fee_msat(self, amount_msat, channel_id, target_pubkey):
-        policy = self.lnd.get_policy(channel_id, target_pubkey)
-        fee_base_msat = self.get_fee_base_msat(policy)
-        fee_rate_milli_msat = int(policy.fee_rate_milli_msat)
-        return fee_base_msat + fee_rate_milli_msat * amount_msat // 1000000
-
-    @staticmethod
-    def get_fee_base_msat(policy):
-        # sometimes that field seems not to be set -- interpret it as 0
-        if hasattr(policy, "fee_base_msat"):
-            return int(policy.fee_base_msat)
-        return int(0)
+        return self.route_extension.add_rebalance_channel(route)
 
     def add_route(self, route):
         if self.route_is_invalid(route):
