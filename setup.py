f1334d08e9b7758ffca2ae1294d3c70b10c5f351
Ioan Bizău <i@ibz.me>
2022-01-12 20:29:41 +0200

    +setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+from setuptools import setup
+
+setup(
+    name='rebalance-lnd',
+    version='2.1',
+    description='A script that can be used to balance lightning channels of a LND node',
+    author='Carsten Otto',
+    author_email='carsten@c-otto.de',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3'
+        ],
+    keywords='lightning, lnd, bitcoin',
+    python_requires='>=3.6, <4',
+    entry_points={
+        'console_scripts' : ['rebalance-lnd=rebalance:main']
+        },
+    project_urls={
+        'Source' : 'https://github.com/C-Otto/rebalance-lnd'
+        }
+)
