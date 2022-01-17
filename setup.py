d6557dede1c5d82f541e09bdbbafaf984ef3554e
Carsten Otto <github@c-otto.de>
2022-01-17 09:19:23 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='rebalance-lnd',
-    version='2.2',
+    version='2.3',
     description='A script that can be used to balance lightning channels of a LND node',
     author='Carsten Otto',
     author_email='bitcoin@c-otto.de',
